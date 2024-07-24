75c9babdd0b16428b120a9a326aa5c16caf50927
SAMSUJJOHA LASKAR <samsujjohalaskar@gmail.com>
2024-07-25 03:19:39 +0530

    websocket api updated with the new one

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -11,7 +11,7 @@ const Chat = ({ currentSession }) => {
 
   useEffect(() => {
     const createClient = () => {
-      const client = new W3CWebSocket("ws://chitchatws.onrender.com");
+      const client = new W3CWebSocket("wss://chitchatws.onrender.com");
       clientRef.current = client;
 
       client.onopen = () => {
