1596f017732ff7ae5cf37d61c03c0fdc7b041ba3
SAMSUJJOHA LASKAR <samsujjohalaskar@gmail.com>
2024-07-24 21:59:27 +0530

    live url added

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:1337/api";
+const API_URL = "https://chitchat-server-ge49.onrender.com/api";
 
 export const register = (username,fullname, phone, email, password) => {
   return axios.post(`${API_URL}/auth/local/register`, {
